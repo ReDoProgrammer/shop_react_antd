15fd1bfce8a4d134ef5caa5c45da72d4b42d8826
redoprogrammer <redoprogrammer@gmail.com>
2023-03-08 17:52:12 +0700

    design menu

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,21 +1,57 @@
 import { Menu } from 'antd';
-import {HomeFilled} from '@ant-design/icons';
+import { HomeFilled } from '@ant-design/icons';
 function Header() {
     return (<div className="header">
         <Menu
             mode='horizontal'
             items={[
                 {
-                    label: <HomeFilled/>,
+                    label: <HomeFilled />,
                     key: 'home'
                 },
                 {
                     label: 'Men',
-                    key: 'men'
+                    key: 'men',
+                    children: [
+                        {
+                            label: "Men's Shirts",
+                            key: 'mens-shirts'
+                        },
+                        {
+                            label:"Men's Shoes",
+                            key:"mens-shoes"
+                        },
+                        {
+                            label:"Men's Watches",
+                            key:"mens watches"
+                        }
+                    ]
                 },
                 {
                     label: 'Women',
-                    key: 'women'
+                    key: 'women',
+                    children:[
+                        {
+                            label:"Women's Dresses",
+                            key:"woments-dresses"
+                        },
+                        {
+                            label:"Women's Shoes",
+                            key:"womens-shoes"
+                        },
+                        {
+                            label:"Women's Watches",
+                            key:"womens-watches"
+                        },
+                        {
+                            label:"Women's Bags",
+                            key:"womens-bags"
+                        },
+                        {
+                            label:"Women's Jewellery",
+                            key:"womens-jewellery"
+                        }
+                    ]
                 },
                 {
                     label: 'Accessories',
