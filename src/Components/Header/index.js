c04a9e53a5f9e24cd4dc3aaf3c366b3783e36872
redoprogrammer <redoprogrammer@gmail.com>
2023-03-08 17:11:37 +0700

    set horizontal mode  to menu

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,23 +1,24 @@
-import {Menu} from 'antd';
+import { Menu } from 'antd';
 function Header() {
-    return (  <div className="header">
+    return (<div className="header">
         <Menu
+            mode='horizontal'
             items={[
                 {
-                    label:'Home',
-                    key:'home'
+                    label: 'Home',
+                    key: 'home'
                 },
                 {
-                    label:'Men',
-                    key:'men'
+                    label: 'Men',
+                    key: 'men'
                 },
                 {
-                    label:'Women',
-                    key:'women'
+                    label: 'Women',
+                    key: 'women'
                 },
                 {
-                    label:'Accessories',
-                    key:'accessories'
+                    label: 'Accessories',
+                    key: 'accessories'
                 }
             ]}
         />
