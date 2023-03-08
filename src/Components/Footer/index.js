e230deafbffd890a10664eddf3236883b778a32a
redoprogrammer <redoprogrammer@gmail.com>
2023-03-08 22:28:40 +0700

    edit footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,6 +1,11 @@
+import Typography from "antd/es/typography/Typography";
+
 function Footer() {
     return ( <div className="footer">
-        <h1>Footer</h1>
+        <Typography.Link href="https://www.youtube.com/watch?v=maTYhCuHEGw&ab_channel=CodeWithAamir" target={'_blank'}>Privacy Policy</Typography.Link>
+        <Typography.Link href="https://www.youtube.com/watch?v=maTYhCuHEGw&ab_channel=CodeWithAamir" target={'_blank'}>Terms & Conditions</Typography.Link>
+        <Typography.Link href="https://www.youtube.com/watch?v=maTYhCuHEGw&ab_channel=CodeWithAamir" target={'_blank'}>Return Policy</Typography.Link>
+        <Typography.Link href="tel:+84911397764" target={'_blank'}>+84911397764</Typography.Link>
     </div> );
 }
 
