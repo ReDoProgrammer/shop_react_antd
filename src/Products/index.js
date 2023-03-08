9d81cdd16ecdb3466bbc9872a67ed26ed5b1591a
redoprogrammer <redoprogrammer@gmail.com>
2023-03-08 18:30:09 +0700

    display products on a list

--- src/Products/index.js
+++ src/Products/index.js
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 import { getAllProducts } from "../API";
+import {List, Card, Image} from 'antd';
 //các kiến thức liên quan tới hook: useState,useEffect
 function Products() {
-    const [Items, setItems] = useState([]);
+    const [items, setItems] = useState([]);
     useEffect(() => {
-        getAllProducts().then(res => {
+        getAllProducts().then(res => {          
             setItems(res.products);
         })
     }, [])// dung mảng trống [] chỉ gọi 1 lần
-    return <div>Items</div>
+    return <div>
+        <List renderItem={(product,index)=>{
+            return <Card title={product.title} key={index} cover={<Image src ={product.thumbnail}/>}>
+
+            </Card>
+        }} dataSource={items}>
+
+        </List>
+    </div>
 }
 
 export default Products;
