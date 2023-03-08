f9466e85601d5a7fc9ebd6381fdf680251dd0e48
redoprogrammer <redoprogrammer@gmail.com>
2023-03-08 19:19:35 +0700

    add loading to button using useState

--- src/Products/index.js
+++ src/Products/index.js
@@ -1,53 +1,58 @@
 import { useEffect, useState } from "react";
-import { getAllProducts,addToCart } from "../API";
-import {List, Card, Image,Typography, Badge, Rate, Button, message} from 'antd';
+import { getAllProducts, addToCart } from "../API";
+import { List, Card, Image, Typography, Badge, Rate, Button, message } from 'antd';
 //các kiến thức liên quan tới hook: useState,useEffect
 function Products() {
     const [items, setItems] = useState([]);
     useEffect(() => {
-        getAllProducts().then(res => {          
+        getAllProducts().then(res => {
             setItems(res.products);
         })
     }, [])// dung mảng trống [] chỉ gọi 1 lần
     return <div>
-        <List 
-        grid={{column:4}}
-        renderItem={(product,index)=>{
-            return  <Badge.Ribbon text={product.discountPercentage} color="pink" className="itemCardBadge">
-            <Card className="itemCard" title={product.title} key={index} 
-            cover={<Image className="itemCardImage" src ={product.thumbnail}/>}
-            actions={[
-                <Rate value={product.rating} allowHalf/>,
-                <AddToCartButton item={product}/>
-            ]}
-            >
-                <Card.Meta title={<Typography.Paragraph>
-                    Price: ${product.price}{" "}
-                    <Typography.Text delete type="danger">
-                        ${parseFloat(product.price + (product.price*product.discountPercentage)/100).toFixed(2)}                       
-                    </Typography.Text>
-                </Typography.Paragraph>}
-                    description={<Typography.Paragraph ellipsis={{rows:2,expandable:true,symbol:"more"}}>{product.description}</Typography.Paragraph>}
-                >
-                    
-                </Card.Meta>
-            </Card>
-            </Badge.Ribbon>
-        }} dataSource={items}>
+        <List
+            grid={{ column: 4 }}
+            renderItem={(product, index) => {
+                return <Badge.Ribbon text={product.discountPercentage} color="pink" className="itemCardBadge">
+                    <Card className="itemCard" title={product.title} key={index}
+                        cover={<Image className="itemCardImage" src={product.thumbnail} />}
+                        actions={[
+                            <Rate value={product.rating} allowHalf />,
+                            <AddToCartButton item={product} />
+                        ]}
+                    >
+                        <Card.Meta title={<Typography.Paragraph>
+                            Price: ${product.price}{" "}
+                            <Typography.Text delete type="danger">
+                                ${parseFloat(product.price + (product.price * product.discountPercentage) / 100).toFixed(2)}
+                            </Typography.Text>
+                        </Typography.Paragraph>}
+                            description={<Typography.Paragraph ellipsis={{ rows: 2, expandable: true, symbol: "more" }}>{product.description}</Typography.Paragraph>}
+                        >
+
+                        </Card.Meta>
+                    </Card>
+                </Badge.Ribbon>
+            }} dataSource={items}>
 
         </List>
     </div>
 }
 
-function AddToCartButton({item}){
-    const addProductToCart = ()=>{
-        addToCart(item.id).then(res=>{
-            message.success(`${item.title} has been added into cart`)
+function AddToCartButton({ item }) {
+    const [loading, setloading] = useState(false);
+    const addProductToCart = () => {
+        setloading(true);
+        addToCart(item.id).then(res => {
+            message.success(`${item.title} has been added to cart`);
+            setloading(false);
         })
     }
-    return <Button type="link"  onClick={()=>{
+    return <Button type="link" onClick={() => {
         addProductToCart();
-    }}>Add to cart</Button>
+    }}
+    loading = {loading}
+    >Add to cart</Button>
 }
 
 export default Products;
